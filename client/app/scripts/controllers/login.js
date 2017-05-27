1fe6f784c2892b95545fdb02e7036bf6e7bc726f
mfprado <pradomflorencia@gmail.com>
2017-05-27 13:14:19 -0300

    shadow item selected at navbar

--- client/app/scripts/controllers/login.js
+++ client/app/scripts/controllers/login.js
@@ -23,6 +23,26 @@ angular.module('underfyApp')
        */
 
   $scope.pageSelected = "views/artists.html";
+  $scope.artistsStatus = "active";
+
+  $scope.artistsSelected = function() {
+        $scope.pageSelected = "views/artists.html";
+        $scope.artistsStatus = "active";
+        $scope.albumsStatus = $scope.songsStatus = $scope.adjustStatus = "";}
+  $scope.adjustSelected = function() {
+        $scope.pageSelected = "views/adjusts.html"
+        $scope.adjustStatus = "active";
+        $scope.albumsStatus = $scope.songsStatus = $scope.artistsStatus = "";}
+  $scope.albumsSelected = function() {
+      $scope.pageSelected = "views/albums.html";
+      $scope.albumsStatus = "active";
+      $scope.adjustStatus  = $scope.songsStatus = $scope.artistsStatus = "";
+  }
+  $scope.songsSelected = function() {
+      $scope.pageSelected = "views/songs.html";
+      $scope.songsStatus = "active";
+      $scope.adjustStatus  = $scope.albumsStatus = $scope.artistsStatus = "";
+  }
 
   var token = $sessionStorage.userData.token;
   $scope.userName = $sessionStorage.userData.user.userName;
