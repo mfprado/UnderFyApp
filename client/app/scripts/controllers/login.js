6722966e52be3de9223dfb2d4d7dc5b90fffdb1b
mfprado <pradomflorencia@gmail.com>
2017-05-30 17:32:55 -0300

    get artists, albums & tracks

--- client/app/scripts/controllers/login.js
+++ client/app/scripts/controllers/login.js
@@ -15,19 +15,20 @@ angular.module('underfyApp')
   $scope.artistsStatus = "active";
 
   $scope.artistsSelected = function() {
-        $scope.pageSelected = "views/artists.html";
-        $scope.artistsStatus = "active";
-        $scope.albumsStatus = $scope.songsStatus =  "";}
+    $scope.pageSelected = "views/artists.html";
+    $scope.artistsStatus = "active";
+    $scope.albumsStatus = $scope.songsStatus =  "";
+  };
   $scope.albumsSelected = function() {
-      $scope.pageSelected = "views/albums.html";
-      $scope.albumsStatus = "active";
-      $scope.songsStatus = $scope.artistsStatus = "";
-  }
+    $scope.pageSelected = "views/albums.html";
+    $scope.albumsStatus = "active";
+    $scope.songsStatus = $scope.artistsStatus = "";
+   };
   $scope.songsSelected = function() {
-      $scope.pageSelected = "views/songs.html";
-      $scope.songsStatus = "active";
-      $scope.albumsStatus = $scope.artistsStatus = "";
-  }
+    $scope.pageSelected = "views/songs.html";
+    $scope.songsStatus = "active";
+    $scope.albumsStatus = $scope.artistsStatus = "";
+  };
 
   $scope.userName = $sessionStorage.userData.user.userName;
 
@@ -46,6 +47,7 @@ angular.module('underfyApp')
       console.log(settings);
 
       $.ajax(settings).done(function (response) {
+          console.log(response);
           $sessionStorage.songs = response.tracks;
       });
   };
@@ -67,34 +69,35 @@ angular.module('underfyApp')
       console.log(settings);
 
       $.ajax(settings).done(function (response) {
+          console.log(response);
           $sessionStorage.albums = response.albums;
       });
   };
   var getArtists = function () {
-          var settings = {
-              "async": true,
-              "crossDomain": true,
-              "url": "https://immense-taiga-71996.herokuapp.com/artists/",
-              "method": "GET",
-              "headers": {
-                  "content-type": "application/x-www-form-urlencoded",
-              },
-              "data": {"token": $sessionStorage.userData.token}
+      var settings = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://immense-taiga-71996.herokuapp.com/artists/",
+          "method": "GET",
+          "headers": {
+              "content-type": "application/x-www-form-urlencoded",
+          },
+          "data": {"token": $sessionStorage.userData.token}
 
 
-          };
+      };
 
-          console.log(settings);
+      console.log(settings);
 
-          $.ajax(settings).done(function (response) {
-              $sessionStorage.albums = response.albums;
-          });
+      $.ajax(settings).done(function (response) {
+          console.log(response);
+          $sessionStorage.artists = response.artists;
+      });
       };
 
   getTracks();
-  console.log($sessionStorage.songs);
-  // getAlbums();  //TODO: implementar en el shared
-  // getArtists(); //TODO: implementar en el shared
+  getAlbums();  //TODO: implementar en el shared
+  getArtists(); //TODO: implementar en el shared
 
 
 }]);
