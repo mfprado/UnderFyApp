cdab82cbc1d8d16be9cd26f23eb714e299a47bb2
mfprado <pradomflorencia@gmail.com>
2017-06-18 17:06:33 -0300

    Refact view

--- client/app/scripts/services/services.js
+++ client/app/scripts/services/services.js
@@ -11,9 +11,8 @@ angular.module('underfyApp').service('Requester', ['$sessionStorage',function ($
             "data": {"token": $sessionStorage.userData.token}
         };
 
-
         this.getArtists = function () {
-            settings.url = urlBase + "/artists";
+            settings.url = urlBase + "/artists/";
             console.log(settings);
 
             $.ajax(settings).done(function (response) {
@@ -23,7 +22,7 @@ angular.module('underfyApp').service('Requester', ['$sessionStorage',function ($
         };
 
         this.getTracks = function () {
-            settings.url = urlBase + "/tracks";
+            settings.url = urlBase + "/tracks/";
             console.log(settings);
 
             $.ajax(settings).done(function (response) {
@@ -33,7 +32,7 @@ angular.module('underfyApp').service('Requester', ['$sessionStorage',function ($
         };
 
         this.getAlbums = function () {
-            settings.url =  urlBase + "/albums";
+            settings.url =  urlBase + "/albums/";
             console.log(settings);
 
             $.ajax(settings).done(function (response) {
@@ -43,13 +42,55 @@ angular.module('underfyApp').service('Requester', ['$sessionStorage',function ($
         };
 
         this.getUsers = function () {
-            settings.url =  urlBase + "/users"
+            settings.url =  urlBase + "/users/";
             console.log(settings);
 
             $.ajax(settings).done(function (response) {
                 console.log(response);
                 $sessionStorage.users = response.users;
             });
+        };
+
+        this.deleteAlbum = function(id){
+            settings.url = urlBase + "/albums/" + id;
+            settings.method = "DELETE";
+            console.log(settings);
+
+            return $.ajax(settings)
+        };
+
+        this.deleteArtist = function (id) {
+            settings.url = urlBase + "/artists/" + id;
+            settings.method = "DELETE";
+            console.log(settings);
+
+            return $.ajax(settings)
+        }
+
+        this.addArtist =  function (name,description,genres,images) {
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://immense-taiga-71996.herokuapp.com/artists",
+                "method": "POST",
+                "headers": {"content-type": "application/x-www-form-urlencoded"},
+                "data": {
+                    "token": $sessionStorage.userData.token,
+                    "name": name,
+                    "description": description,
+                    "genres": genres,
+                    "images": images
+                }
+            };
+            settings.url = urlBase + "/artists";
+            settings.method = "POST";
+
+            console.log(settings);
+
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+            });
+
         };
 
     }]);
