24c973b704b53dfd0a372835d0c4fde9b56cd5e7
mfprado <pradomflorencia@gmail.com>
2017-06-22 03:28:55 -0300

    upload file to appsvr

--- client/app/scripts/controllers/uploadTrack.js
+++ client/app/scripts/controllers/uploadTrack.js
@@ -1,49 +1,44 @@
-var app = angular.module('uploadApp',['ngStorage']);
-
-    app.directive('progressBar', [
-        function () {
-            return {
-                link: function ($scope, el, attrs) {
-                    $scope.$watch(attrs.progressBar, function (newValue) {
-                        el.css('width', newValue.toString() + '%');
-                    });
-                }
-            };
-        }
-    ]);
-
-
-
-
-    app.controller('uploadController',['$scope','$sessionStorage',function ($scope, $sessionStorage) {
+angular.module('uploadApp',['ngStorage','lr.upload']).controller('uploadController',['$scope','$sessionStorage','upload',function ($scope, $sessionStorage, upload) {
 
     var token = $sessionStorage.userData.token;
+    $scope.model = {};
+    $scope.selectedFile = [];
+    $scope.uploadProgress = 0;
 
-    console.log(token );
 
-    $scope.upload = function (id) {
-        var form = new FormData();
-        form.append("upload.ashx",$scope.file);
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "http://ec2-54-200-74-89.us-west-2.compute.amazonaws.com/appsvr/song?id_song="+id,
-            "method": "PUT",
-            "headers": {
+    $scope.uploadFile = function (id) {
+        //Envio del file al appsvr
+        var file = $scope.selectedFile[0];
+        $scope.upload = upload({
+
+            url: "http://ec2-54-200-74-89.us-west-2.compute.amazonaws.com/appsvr/song?id_song=" + id,
+            method: 'PUT',
+            async: true,
+            crossDomain: true,
+            processData: false,
+            contentType:false,
+            mimeType: "multipart/form-data",
+            headers: {
                 "Authorization": "basic token"
             },
-            "processData": false,
-            "contentType": false,
-            "mimeType": "multipart/form-data",
-            "data": form,
-            "success": $scope.success()
-        };
-        console.log(settings);
-
-        $.ajax(settings).done(function (response) {
-            console.log(response);
+            data: file
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
         });
     };
+
+
+
+    $scope.onFileSelect = function ($files) {
+        $scope.uploadProgress = 0;
+        $scope.selectedFile = $files;
+    };
+
 
     $scope.success = function() {
 //                window.close();
@@ -56,6 +51,7 @@ var app = angular.module('uploadApp',['ngStorage']);
     };
 
     $scope.create = function() {
+        //Pedido id al shared
         if( tname.value && albumId.value && artistsIds.value && file){
 
             var settings = {
@@ -74,7 +70,7 @@ var app = angular.module('uploadApp',['ngStorage']);
                 },
                 "success" : function (response) {
                     console.log(response);
-                    $scope.upload(response.id);
+                    $scope.uploadFile(response.id);
                 }
             };
             console.log(settings);
